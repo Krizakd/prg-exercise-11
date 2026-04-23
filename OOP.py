2005177be4ba89d956f93e54b302094873cb9865
krizakd63@gmail.com <krizakd63@gmail.com>
2026-04-23 16:28:54 +0200

    treti

--- OOP.py
+++ OOP.py
@@ -23,13 +23,36 @@ class StudentsGrades:
         else:
             return "F"
 
+    def find(self, find):
+        misto = []
+        for id, cislo in enumerate(self.scores):
+            if cislo == find:
+                misto.append(id)
+        return misto
 
 
-results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
+    def get_sorted(self):
+        novy_seznam = self.scores.copy()
+        zmena = True
+        while zmena:
+            zmena = False
 
-print(results.count())          # 9
-print(results.get_by_index(2))  # 91
-print(results.scores)
-print(results.get_grade(3))# [85, 42, 91, 67, 50, 73, 100, 38, 58]
+            for i in range(len(novy_seznam) - 1):
+                if novy_seznam[i + 1] < novy_seznam[i]:
+                    novy_seznam[i + 1], novy_seznam[i] = novy_seznam[i], novy_seznam[i + 1]
+                    zmena = True
+        return novy_seznam
+
+if __name__ == '__main__':
+
+
+    results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
+
+    print(results.count())          # 9
+    print(results.get_by_index(2))  # 91
+    print(results.scores)
+    print(results.get_grade(3))# [85, 42, 91, 67, 50, 73, 100, 38, 58]
+    print(results.find(100))
+    print(results.get_sorted())
 
 
